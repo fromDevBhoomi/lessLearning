a2a106e43c5072b876bb44b3ac33d071c97e6cba
fromDevBhoomi <manojnegi.max@gmail.com>
2017-09-07 12:35:40 +0530

    Styles task added for compling Sass to CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,29 @@
+/*
+ *	GulpScript Task: Minifying JS, Compiling Sass to CSS, Watching for changes in files 
+ */
+
 var gulp = require('gulp'),
 	uglify = require('gulp-uglify'),
-	sass = require('gulp-ruby-sass');
+	//sass = require('gulp-ruby-sass');
+	sass = require('gulp-sass');
 
 gulp.task('minifyJS', function(){
 	gulp.src('js/*.js')
-	.pipe(uglify())
-	.pipe(gulp.dest('minjs'));
+		.pipe(uglify())
+		.pipe(gulp.dest('minjs'));
 });
 
-gulp.task('watch', function(){
-	gulp.watch('js/*.js', ['minifyJS'])
+
+gulp.task('styles', function(){
+	//console.log('Styles task working correctly');
+	gulp.src('sass/**/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('css'));
 });
 
-gulp.task('default', ['minifyJS', 'watch']);
+gulp.task('watchChanges', function(){
+	gulp.watch('sass/**/*.scss', ['styles']);
+	gulp.watch('js/*.js', ['minifyJS']);
+});
 
-//console.log(sass);
+gulp.task('default', ['minifyJS', 'styles', 'watchChanges']);
